9675f0a106f14bc95503aa4382399ac796415b4a
ddle <ddle@pdx.edu>
2012-12-18 05:15:39 -0800

    add makefile

--- blink/main.s
+++ blink/main.s
@@ -1,56 +1,60 @@
+@ "Bare Metal" Raspberry pi: blink status led 
+@ by Dung Le
+@------------------------------------------------------------------
 
-;@ ------------------------------------------------------------------
-;@ ------------------------------------------------------------------
 
-.equ GPFSEL1, 0x20200004
-.equ GPSET0,  0x2020001C
-.equ GPCLR0,  0x20200028
+@ GPIO - status LED
+.equ GPFSEL1,    0x20200004
+.equ GPSET0,     0x2020001C
+.equ GPCLR0,     0x20200028
+.equ S_LED,              16
 
-.globl _start
-_start:
-    mov sp,#0x8000
+    BL led_init
+LOOP:
+    BL led_on
+    BL delay_1s
+    BL led_off
+    BL delay_1s
+    B LOOP
+
+@==================== initialize status LED ====================================
+led_init:
+    STMFD    R13!, {R0-R3, LR}     @ save registers, R14
+    LDR R0,=GPFSEL1
+    LDR R1, [R0]    
+    @ GPFSEL1 &= ~(3<<19); // CLEAR 20,19 (AF 00X: gpio)
+    MOV R2, #3
+    MVN R2, R2, lsl #19
+    AND R1, R1, R2
+    @ GPFSEL1 |= 1<<18;    // SET 18  (AF 001: gpio-output)
+    MOV R2, #1
+    MOV R2, R2, lsl #18
+    ORR R1, R1, R2
+    STR R1, [R0]  
+    LDMFD    R13!, {R0-R3, PC}     @ restore resister and return
     
-    @translating ...    
-        
-    ldr r0,=GPFSEL1
-    ldr r1, [r0]
-    
-    @ra &= ~(7<<18); // alternate function 000: gpio-input
-    mov r2, #7
-    mvn r2, r2, lsl #18
-    and r1, r1, r2
+@========================== LED ON/OFF =========================================
 
-    @ ra |= 1<<18;   // alternate function 001: gpio-output
-    mov r2, #1
-    mov r2, r2, lsl #18
-    orr r1, r1, r2
-        
-    str r1, [r0]    
-    
-blink:
-    bl on
-    bl delay_1s
-    bl off
-    bl delay_1s
-	b blink            
-off:
-    mov r2, #1
-    mov r2, r2, lsl #16
-	ldr r0,=GPSET0
-    str r2, [r0]
-	bx lr
-on:
-    mov r2, #1
-    mov r2, r2, lsl #16
-	ldr r0,=GPCLR0
-    str r2, [r0]
-	bx lr
-	
+led_off:
+    STMFD    R13!, {R0, R2, LR}    @ save registers, R14
+    MOV R2, #1
+    MOV R2, R2, lsl #S_LED
+    LDR R0,=GPSET0
+    STR R2, [R0]
+    LDMFD    R13!, {R0, R2, PC}    @ restore resister and return
+
+led_on:
+    STMFD    R13!, {R0, R2, LR}    @ save registers, R14
+    MOV R2, #1
+    MOV R2, R2, lsl #S_LED
+    LDR R0,=GPCLR0
+    STR R2, [R0]
+    LDMFD    R13!, {R0, R2, PC}    @ restore resister and return	
+
 delay_1s:
-	LDR	R1, =0x01FFFFF		@ init register counter value ~ 1 second
-	DELAY_LOOP:				@ REPEAT
-	SUBS	R1, R1, #1		@ decrease counter by 1 and set flag
-	BNE	DELAY_LOOP			@ UNTIL counter = 0 (Z flag set)
-	bx lr
-
+	LDR	R1, =0xD00000		
+	DELAY_LOOP:				
+	SUBS	R1, R1, #1		
+	BNE	DELAY_LOOP			
+	BX LR
 
